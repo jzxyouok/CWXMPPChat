116416570e0dee36ccea088561395dd86ed00025
chenwei <wei18810109052@163.com>
2016-05-28 00:17:21 +0800

    添加Tabbar文件

---
+++ CWXMPPChat/MainClass/Base/CWChatTabBarContoller.swift
@@ -0,0 +1,13 @@
+//
+//  CWChatTabBarContoller.swift
+//  CWXMPPChat
+//
+//  Created by chenwei on 16/5/28.
+//  Copyright © 2016年 chenwei. All rights reserved.
+//
+
+import UIKit
+
+class CWChatTabBarContoller: UITabBarController {
+
+}
